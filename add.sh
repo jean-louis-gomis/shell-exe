ee14fc7370d83e1b17c717894e1771ce1db88afe
jean-louis-gomis <jean-louis.gomis@laplateforme.io>
2022-09-28 15:25:12 +0200

    premières conditions

---
+++ add.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+echo "40 2"
+echo "Result"  
+a=40
+b=2
+add= expr $a + $b
+
---
+++ hello_bye.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+ if [ Hello ]
+then
+echo "Bonjour, je suis un script!"
+elif [ Bye ]
+then
+echo "Au revoir et bonne journée"
+fi
